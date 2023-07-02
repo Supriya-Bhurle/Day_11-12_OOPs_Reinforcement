97d8401f506e1febe4d5ef136ac30c88174b8bda
Supriya Bhurle <supriyabhurle28@gmail.com>
2023-07-02 18:11:29 +0530

    Added : Day 11 & 12 object oriented programming practice problem Added

--- src/com/bridgelabz/account.java
+++ src/com/bridgelabz/account.java
@@ -1,2 +1,20 @@
-package com.bridgelabz;public class account {
-}
+package com.bridgelabz;
+
+public class account {
+    int accountBalance;
+    void debit(int debitAmount){
+        if (this.accountBalance < debitAmount){
+            System.out.println("Debit amount exceeds account balance");
+        }
+        else {
+            this.accountBalance = this.accountBalance - debitAmount;
+            System.out.println("remaining balance is " + this.accountBalance);
+        }
+    }
+
+    public static void main(String[] args) {
+        account union = new account();
+        union.accountBalance = 10000;
+        union.debit(5000);
+        union.debit(10000);
+}}
